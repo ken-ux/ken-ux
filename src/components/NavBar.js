4185e3a29b5a8e8e96a31162f2f85b817d6922be
Kenny Nguyen <sokenns@gmail.com>
2021-03-15 02:44:59 -0700

    Import Navbar component to customize

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+
 function NavBar(props) {
-  const navBarStyle = {
-    padding: 48,
-    fontFamily: "-apple-system, Roboto, sans-serif, serif"
-  }
   return (
-    <navbar style={navBarStyle}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/resume">Resume</Link>
-    </navbar>
+    <Navbar expand="lg">
+        <Navbar.Brand href="/">Placeholder</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
   )
 }
 export default NavBar
