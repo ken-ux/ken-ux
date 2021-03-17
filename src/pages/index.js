b3c9bdb0e9e63d44aa7126972b0379da6ab099cf
Kenny Nguyen <sokenns@gmail.com>
2021-03-16 17:12:10 -0700

    Add lottie dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import * as React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/layout";
 import Card from "../components/ProjectCards";
+import "@lottiefiles/lottie-player";
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
   integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
   crossorigin="anonymous"
-/>
+/>;
 
 // styles
 const pageStyles = {
@@ -19,6 +20,10 @@ const headingStyles = {
   marginBottom: 64,
   maxWidth: 600
 }
+const lottieStyles = {
+  maxWidth: 300,
+  maxHeight: 300
+}
 
 // markup
 const IndexPage = () => {
@@ -28,6 +33,15 @@ const IndexPage = () => {
       <Layout>
         <div style={pageStyles}>
           <div style={headingStyles}>
+            <lottie-player
+              src="https://assets7.lottiefiles.com/packages/lf20_micjhd5b.json"
+              background="transparent"
+              speed="1"
+              style={lottieStyles}
+              loop
+              autoplay
+            >
+            </lottie-player>
             <h1>
               Hello! I'm Kenny.
               <br />
