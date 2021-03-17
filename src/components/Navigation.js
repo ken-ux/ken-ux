cff1cef25c17e335ee97db76bb0361e9b9a3576a
Kenny Nguyen <sokenns@gmail.com>
2021-03-16 17:11:58 -0700

    Syntax

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from 'react-bootstrap/Navbar';
 
 export default function Navigation(props) {
