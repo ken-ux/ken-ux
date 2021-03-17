ff0566c2aaea4c08ab2c502d943c59ce21b3cc6d
Kenny Nguyen <sokenns@gmail.com>
2021-03-17 00:27:19 -0700

    Hero text edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Row, Col } from 'react-bootstrap';
 import Layout from "../components/layout";
 import Card from "../components/ProjectCards";
 import "@lottiefiles/lottie-player";
@@ -12,48 +13,58 @@ import "@lottiefiles/lottie-player";
 
 // styles
 const pageStyles = {
-  padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif"
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 600
-}
-const lottieStyles = {
-  maxWidth: 300,
-  maxHeight: 300
 }
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
+    <Layout>
       <title>Kenny Nguyen | Projects</title>
-      <Layout>
-        <div style={pageStyles}>
-          <div style={headingStyles}>
-            <lottie-player
-              src="https://assets7.lottiefiles.com/packages/lf20_micjhd5b.json"
-              background="transparent"
-              speed="1"
-              style={lottieStyles}
-              loop
-              autoplay
-            >
-            </lottie-player>
-            <h1>
-              Hello! I'm Kenny.
+        <div className="px-5" style={pageStyles}>
+          <Row className="align-items-center">
+            <Col>
+              <lottie-player
+                src="https://assets7.lottiefiles.com/packages/lf20_micjhd5b.json"
+                background="transparent"
+                speed="1"
+                loop
+                autoplay
+              >
+              </lottie-player>
+            </Col>
+            <Col>
+              <h1>
+                Hello, I'm Kenny!
+                <br />
+              </h1>
+              <h2>
+                A UX designer and front-end developer
+                <br />
+                based in Seattle. Accessibility | Inclusion | Systematic Design
+              </h2>
               <br />
-            </h1>
-            <h2>
-              A UX designer and front-end developer based in Seattle.
-            </h2>
-          </div>
+              <h3>
+                View my portfolio down below.{" "}
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  width="16" 
+                  height="16" 
+                  fill="currentColor" 
+                  class="bi bi-arrow-down-circle-fill" 
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                </svg>
+              </h3>
+            </Col>
+          </Row>
+          <Card />
+          <Card />
+          <Card />
           <Card />
         </div>
-      </Layout>
-    </main>
+    </Layout>
   )
 }
 
