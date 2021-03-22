4c8a283048f9c5a14cbad5a3b3e254efefa635b1
Kenny Nguyen <sokenns@gmail.com>
2021-03-22 03:53:44 -0700

    Add spacing for cards

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -3,16 +3,14 @@ import Card from "react-bootstrap/Card";
 
 export default function ProjectCards(props) {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+        <Card border={props.border} className="my-3 mx-5">
+            <Card.Body style={{height: "200px"}}>
+                <Card.Title>{props.title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
         </Card>
     )
