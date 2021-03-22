823148b1783c6a8f99168a867444480b542c9411
Kenny Nguyen <sokenns@gmail.com>
2021-03-22 03:54:36 -0700

    Import global css, start making projects body

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/global.css";
 import { Row, Col } from 'react-bootstrap';
 import Layout from "../components/Layout";
 import Card from "../components/ProjectCards";
@@ -13,17 +14,17 @@ import Lottie from 'react-lottie-player';
 />;
 
 // styles
-const pageStyles = {
-  fontFamily: "-apple-system, Roboto, sans-serif, serif"
+const heroStyles = { // To make hero text full length of page
+  height: "95vh"
 }
 
 // markup
 const IndexPage = () => {
   return (
     <Layout>
-      <title>Kenny Nguyen | Projects</title>
-        <div className="px-5" style={pageStyles}>
-          <Row className="align-items-center">
+      <title>Kenny Nguyen | Portfolio</title>
+        <div>
+          <Row className="align-items-center" style={heroStyles}>
             <Col>
               <Lottie
                 loop
@@ -33,23 +34,22 @@ const IndexPage = () => {
             </Col>
             <Col>
               <h1>
-                Hello, I'm Kenny!
+                Kenny Nguyen
                 <br />
               </h1>
               <h2>
-                A UX designer and front-end developer
+                UX Designer | Front-End Developer.
                 <br />
-                based in Seattle. Accessibility | Inclusion | Systematic Design
               </h2>
               <br />
               <h3>
+                A curious thinker passionate about applying an equity-focused 
+                lens to the <span style={{ color: "blue" }}>designs</span> shaping our communities.
+                <br />
+                <br />
                 View my portfolio down below.{" "}
-                <svg 
-                  xmlns="http://www.w3.org/2000/svg" 
-                  width="16" 
-                  height="16" 
-                  fill="currentColor" 
-                  class="bi bi-arrow-down-circle-fill" 
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" 
+                  fill="currentColor" class="bi bi-arrow-down-circle-fill" 
                   viewBox="0 0 16 16"
                 >
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
@@ -58,14 +58,14 @@ const IndexPage = () => {
             </Col>
           </Row>
           <Row> {/* Use function to repeat cards instead of pasting manually */}
-            <Col>
-              <Card />
+            <Col className="my-projects my-3">
+              <h2>Projects</h2>
             </Col>
-            <Col>
-              <Card />
-            </Col>
-            <Col>
-              <Card />
+            <Col xs={9}>
+                <Card border="primary" title="Visualizing Diversity in Higher Education"/>
+                <Card border="success" title="Pura"/>
+                <Card border="warning" title="Placeholder"/>
+                <Card border="danger" title="Placeholder"/>
             </Col>
           </Row>
         </div>
