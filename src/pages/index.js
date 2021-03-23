6d018d33d609eac8cc97c2acd467e30756d044ca
Kenny Nguyen <sokenns@gmail.com>
2021-03-22 19:24:09 -0700

    Import Link for routing, adjust hero spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Layout from "../components/Layout";
 import Card from "../components/ProjectCards";
 import animationData from "../animations/lottieanim.json";
 import Lottie from 'react-lottie-player';
+import { Link } from "gatsby";
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
@@ -15,7 +16,7 @@ import Lottie from 'react-lottie-player';
 
 // styles
 const heroStyles = { // To make hero text full length of page
-  height: "95vh"
+  height: "92vh"
 }
 
 // markup
@@ -32,40 +33,49 @@ const IndexPage = () => {
                 play
               />
             </Col>
-            <Col>
+            <Col style={{ paddingRight: "50px" }}>
               <h1>
                 Kenny Nguyen
                 <br />
               </h1>
               <h2>
-                UX Designer | Front-End Developer.
+                UX Designer | Software Engineer
                 <br />
               </h2>
               <br />
-              <h3>
-                A curious thinker passionate about applying an equity-focused 
-                lens to the <span style={{ color: "blue" }}>designs</span> shaping our communities.
+              <h4>
+                A curious thinker with an <span style={{ color: "#FF5349" }}>equity-focused</span> lens for the
+                <span style={{ color: "green" }}> designs</span> shaping global communities.
+                <br />
+                <br />
+                I'm a <span style={{ color: "#00A4EF" }}>scientist-turned-designer</span> drawn to 
+                addressing social issues through interaction design. <a href="/about"><em>Here's my story.</em></a>
                 <br />
                 <br />
                 View my portfolio down below.{" "}
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" 
-                  fill="currentColor" class="bi bi-arrow-down-circle-fill" 
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
-                </svg>
-              </h3>
+                <Link to="#projects">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" 
+                    fill="currentColor" class="bi bi-arrow-down-circle-fill" 
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                  </svg>
+                </Link>
+              </h4>
             </Col>
           </Row>
           <Row> {/* Use function to repeat cards instead of pasting manually */}
-            <Col className="my-projects my-3">
+            <Col className="my-projects my-3" id="projects">
               <h2>Projects</h2>
             </Col>
             <Col xs={9}>
-                <Card border="primary" title="Visualizing Diversity in Higher Education"/>
-                <Card border="success" title="Pura"/>
-                <Card border="warning" title="Placeholder"/>
-                <Card border="danger" title="Placeholder"/>
+                <Card
+                  border="primary"
+                  title="Visualizing Diversity in Higher Education"
+                  subtitle="Data Visualization, Qualitative User Testing"/>
+                <Card border="success" title="COVID Visualization Redesign"/>
+                <Card border="warning" title="Pura"/>
+                <Card border="danger" title="Case Study"/>
             </Col>
           </Row>
         </div>
