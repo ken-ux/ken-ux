756d13008dddec71b187ba7535ccd0d5787e6cd2
Kenny Nguyen <sokenns@gmail.com>
2021-03-22 19:23:20 -0700

    Adjust vertical spacing, add in props for subtitle

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -3,10 +3,10 @@ import Card from "react-bootstrap/Card";
 
 export default function ProjectCards(props) {
     return (
-        <Card border={props.border} className="my-3 mx-5">
+        <Card border={props.border} className="my-4 mx-5">
             <Card.Body style={{height: "200px"}}>
                 <Card.Title>{props.title}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">{props.subtitle}</Card.Subtitle>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
