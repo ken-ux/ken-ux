7d771cc030539abbf212921528031e6c0c2057e4
Kenny Nguyen <sokenns@gmail.com>
2021-03-25 01:22:41 -0700

    Remove card border colors

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -3,7 +3,7 @@ import Card from "react-bootstrap/Card";
 
 export default function ProjectCards(props) {
     return (
-        <Card border={props.border} className="my-4 mx-5">
+        <Card border="secondary" className="my-4 mx-5">
             <Card.Body style={{height: "200px"}}>
                 <Card.Title>{props.title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{props.subtitle}</Card.Subtitle>
--- src/pages/index.js
+++ src/pages/index.js
@@ -55,7 +55,7 @@ const IndexPage = () => {
                 View my portfolio down below.{" "}
                 <Link to="#projects">
                   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" 
-                    fill="currentColor" class="bi bi-arrow-down-circle-fill" 
+                    fill="currentColor" className="bi bi-arrow-down-circle-fill" 
                     viewBox="0 0 16 16"
                   >
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
@@ -70,12 +70,11 @@ const IndexPage = () => {
             </Col>
             <Col xs={9}>
                 <Card
-                  border="primary"
                   title="Visualizing Diversity in Higher Education"
                   subtitle="Data Visualization, Qualitative User Testing"/>
-                <Card border="success" title="COVID Visualization Redesign"/>
-                <Card border="warning" title="Pura"/>
-                <Card border="danger" title="Case Study"/>
+                <Card title="COVID Visualization Redesign"/>
+                <Card title="Pura"/>
+                <Card title="Case Study"/>
             </Col>
           </Row>
         </div>
