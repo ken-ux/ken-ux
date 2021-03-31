902362ce993abbd30cddf668cc1e0d5f7eb935cf
Kenny Nguyen <sokenns@gmail.com>
2021-03-31 10:25:58 -0700

    Edit stylesheet import

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-  integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-  crossorigin="anonymous"
-/>;
 
 const AboutPage = () => {
     return (
