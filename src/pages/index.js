9005b8105d63e506c1409f7bdf2695a4c6e147ee
Kenny Nguyen <sokenns@gmail.com>
2021-07-09 02:34:54 -0700

    add cards, center content

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import Link from '@material-ui/core/Link';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 // import { Link } from "gatsby";
 import profile from '../images/profile.jpeg';
 
@@ -23,13 +27,16 @@ const useStyles = makeStyles((theme) => ({
   //   textAlign: 'center',
   //   color: theme.palette.text.secondary,
   // },
-  heroStyle:
-  {
+  heroStyle: {
     padding: theme.spacing(8)
   },
-  downStyle:
-  {
+  downStyle: {
     marginTop: theme.spacing(10),
+  },
+  profileStyle: {
+    width: "80%",
+    borderRadius: "50%",
+    marginLeft: "10%",
   },
 }));
 
@@ -59,23 +66,47 @@ const IndexPage = () => {
           </Typography>
         </Grid>
         <Grid item xs={6}>
-          <img src={profile} className="profile" alt="profile" />
+          <img src={profile} className={classes.profileStyle} alt="profile" />
         </Grid>
       </React.Fragment>
     );
   }
 
-  function FormCards() {
+  function FormCard() {
     return (
       <React.Fragment>
+        <Grid item xs={6}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Contemplative Reptile"
+                height="300"
+                image={profile}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                  across all continents except Antarctica
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
       </React.Fragment>
     );
   }
 
   return (
     <Layout className={classes.root}>
-      <Grid container spacing={1} className={classes.heroStyle}>
-        <Grid container item xs={12} spacing={3}>
+
+      {/* Hero */}
+      <Grid container spacing={1} className={classes.heroStyle} justifyContent="center">
+        <Grid container item xs={12} spacing={3} alignItems="center">
           <FormHero />
         </Grid>
         <Grid container xs={12} spacing={3} direction="column" alignItems="center" className={classes.downStyle}>
@@ -83,7 +114,15 @@ const IndexPage = () => {
           <KeyboardArrowDownIcon />
         </Grid>
       </Grid>
-      <FormCards />
+
+      {/* Cards */}
+      <Grid container spacing={1} justifyContent="center">
+        <Grid container item xs={12} spacing={6}>
+          <FormCard />
+          <FormCard />
+        </Grid>
+      </Grid>
+
     </Layout>
   )
 }
